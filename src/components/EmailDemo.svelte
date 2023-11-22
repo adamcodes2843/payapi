65d34a87ddb98b9cb6d89c7e70f038821afe17e0
Adam Childers <adamcodes2843@gmail.com>
2023-11-21 23:53:40 -0600

    second commit

---
+++ src/components/EmailDemo.svelte
@@ -0,0 +1,38 @@
+<script>
+    let email = "";
+    let demoScheduled = false;
+    function scheduleDemo() {
+
+    }
+</script>
+
+<div>
+    <input 
+        type="text" 
+        bind:value={email}
+        placeholder="Enter email address"
+    >
+    <button type="button" on:click={() => demoScheduled = true} class="schedule-button">Schedule a Demo</button>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+    input {
+        
+        font-family: 'Public Sans', sans-serif;
+        font-weight: 700;
+        color: var(--sanJuanBlue);
+        border-radius: 20px;
+        border: none;
+        padding: 14px 20px;
+        font-size: 15px;
+        box-shadow: 2px 2px 8px var(--sanJuanBlue);
+    }
+    input:focus {
+        outline: none;
+    }
+</style>
---
+++ src/components/SmallMenu.svelte
@@ -0,0 +1,75 @@
+<script>
+    let showMenu = false;
+    function menu() {
+        showMenu = !showMenu;
+    }
+</script>
+
+<button type='button' on:click={menu}>
+    <img src='../../assets/shared/mobile/menu.svg' alt="menu" class="menu"/>
+</button>
+
+{#if showMenu}
+    <div class="side-bar">
+        <button type="button" on:click={menu}> 
+            <img src='../../assets/shared/mobile/close.svg' alt='close menu' class='close' />
+        </button>
+        <ul class="side-bar-list">
+            <li class='menu-link'>
+                <a href="/pricing">Pricing</a>
+            </li>
+            <li class='menu-link'>
+                <a href="/about">About</a>
+            </li>
+            <li class='menu-link'>
+                <a href="/contact">Contact</a>
+            </li>
+            <li class="schedule-button">
+                <a href="/contact">Schedule a Demo</a>
+            </li>
+        </ul>
+    </div>
+{/if}
+
+<style>
+    .side-bar {
+        background-color: rgba(27, 38, 47, 0.99);
+        position: fixed;
+        width: 65%;
+        height: 100vh;
+        top: 0;
+        right: 0;
+        color: var(--linkWaterWhite);
+        padding: 2.2rem 1.5rem;
+        text-align: right;
+    }
+    .side-bar-list {
+        margin-top: 1.5rem;
+        display: flex;
+        flex-direction: column;
+        list-style-type: none;
+        border-top: 1px solid rgba(251, 252, 254, 0.2);
+        align-items: center;
+        gap: 2rem;
+        padding: 2.5rem 0;
+        font-size: 20px;
+        font-weight: 700;
+    }
+    .menu-link {
+        opacity: 70%;
+    }
+    a {
+        color: var(--linkWaterWhite)
+    }
+    button {
+        border: none;
+        background: none;
+    }
+    button:hover {
+        cursor: pointer;
+    }
+    .menu {
+        height: 17px;
+        width: 28px;
+    }
+</style>
